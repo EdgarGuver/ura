4ffec512c3d209cfa7ad308c4f57af2816fd9f8a
Yevhen Borysevych <drunkedsneakers@gmail.com>
2017-05-14 01:03:31 +0300

    Change web-page

--- helloworld3.php
+++ helloworld3.php
@@ -3,7 +3,7 @@
 echo ("Hello World<br\>");
 
 // Читаем страницу из интернета
-$string = file_get_contents('https://habrahabr.ru/hub/bitrix/');
+$string = file_get_contents('https://habrahabr.ru/post/323152/');
 echo($string);
 
 ?>
