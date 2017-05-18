7e9a0ebb7dcddafaa3ad10449fe89c1404f16bc1
Yevhen Borysevych <drunkedsneakers@gmail.com>
2017-05-19 01:21:30 +0300

    upgrade. saving into db

--- open.php
+++ open.php
@@ -1,14 +1,21 @@
 <?php
-$f = fopen("quest.txt", "r"); // Открываем файл в режиме чтения
-if ($f)
-{
-	while (!feof($f))
-	{
-		$mytext = fgets($f, 999);
-		echo $mytext."<br />";
-	}
-}
-else echo "Ошибка при открытии файла";
+//$f = fopen("quest.txt", "r"); // Открываем файл в режиме чтения
+//if ($f)
+//{
+//	while (!feof($f))
+//	{
+//		$mytext = fgets($f, 999);
+//		echo $mytext."<br />";
+//	}
+//}
+//else echo "Ошибка при открытии файла";
 
+
+$mysqli = new mysqli("localhost", "root", "", "quest");
+$result_set = $mysqli->query("SELECT * FROM `quest`");
+while ($row = $result_set->fetch_assoc()) {
+	print_r($row);
+	echo "<br />";
+}
 
 ?>
--- post.php
+++ post.php
@@ -4,10 +4,17 @@ $quest1=$_POST['quest1'];
 $quest2=$_POST['quest2'];
 $quest3=$_POST['quest3']; 
 
-$f = fopen("quest.txt", "a+");
-fwrite($f,"Ответ 1: $quest1 <br /><br />Ответ 2: $quest2 <br /><br />Ответ 3: $quest3");
-fclose($f);
+//$f = fopen("quest.txt", "a+");
+//fwrite($f,"Ответ 1: $quest1 <br /><br />Ответ 2: $quest2 <br /><br />Ответ 3: $quest3");
+//fclose($f);
 
+
+$quest1 = htmlspecialchars($quest1);
+$quest2 = htmlspecialchars($quest2);
+$quest3 = htmlspecialchars($quest3);
+
+$mysqli = new mysqli("localhost", "root", "", "quest");
+$mysqli->query("INSERT INTO `quest` (`quest1`, `quest2`, `quest3`) VALUES ('$quest1', '$quest2', '$quest3')");
 header('Location: /demo2/questionansver.html')
 //echo "Ответы сохранены";
 ?>
